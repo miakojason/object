6223b134b2418cb55963b8b7a5a18b98f6aac3de
miakojason <miakojason@gmail.com>
2023-11-24 10:38:55 +0800

    介紹建構式

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 class Animal{
-    public $name;
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
 
     public function setName($name){
         $this->name=$name;
@@ -13,14 +18,14 @@ class Animal{
 
 }
 
-$animal=new Animal; //實例化 instant
+$animal=new Animal('阿明'); //實例化 instant
 
 echo '顯示名稱:'.$animal->getName();
 echo "<br>";
 $animal->setName('小花');
 echo '顯示名稱:'.$animal->getName();
 echo "<br>";
-$animal->name='阿中';
-echo '顯示名稱:'.$animal->name;
+/* $animal->name='阿中';
+echo '顯示名稱:'.$animal->name; */
 echo "<br>";
 
